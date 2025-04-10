8ba8f4d0dcd36f9a0642cba2f1ea6af06bb0f6fa
Tim McMaster <tim@mcmaster.id.au>
2025-04-10 11:35:35 +1000

    Added a LogToFile log listener to log to a log file.

--- lib/wt_logging.dart
+++ lib/wt_logging.dart
@@ -2,6 +2,7 @@ library wt_logging;
 
 export 'package:logger/logger.dart';
 
+export 'src/log_to_file.dart';
 export 'src/logging.dart';
 export 'src/user_log.dart';
 export 'src/user_log_store.dart';
